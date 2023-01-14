2d237a19780b9ae27e8ba566a0a6861c038bc9ed
Janice Thomas <janice.thomas@outlook.com>
2023-01-14 13:08:46 +0530

    Added practice code files

---
+++ data.py
@@ -0,0 +1,15 @@
+
+question_bank =  [
+    {
+        'question' : 'What do you understand by the term AI?',
+        'file' : 'q1.csv',
+        'hint' : 'AI is being used in our chatbot'
+    }
+]
+
+replies = [
+    'Okay...',
+    'Fine....',
+    'Hm...',
+    'I see....'
+]
