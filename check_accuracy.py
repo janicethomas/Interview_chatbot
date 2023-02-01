7928338649cb094b1b3f06d5af3c81cd4f49397a
Janice Thomas <janice.thomas@outlook.com>
2023-02-01 22:03:34 +0530

    added files

--- check_accuracy.py
+++ check_accuracy.py
@@ -7,13 +7,14 @@ tv = TfidfVectorizer()
 model = svm.SVC()
 
 # Read csv
-possible_ans = pd.read_csv('q1_copy.csv')
+possible_ans = pd.read_csv('q5.csv')
 total_rows = len(possible_ans)
 ratio = 1 - (total_rows-1)/total_rows
 
 # Split data into train and test
 ans = possible_ans['Ans']
 label = possible_ans["Label"]
+print(ans)
 ans_train, ans_test,label_train, label_test = train_test_split(ans,label,test_size = ratio, shuffle = True)
 
 # Train data
@@ -23,6 +24,6 @@ model.fit(features,label_train)
 # Check accuracy with test data
 features_test = tv.transform(ans_test)
 # features_test = tv.transform(pd.Series("I don't know"))
-print("Accuracy: {}".format(model.score(features_test,label_test)))
-print("Prediction: ", model.predict(features_test))
-print("Actual score: ",label_test.unique())
+# print("Accuracy: {}".format(model.score(features_test,label_test)))
+# print("Prediction: ", model.predict(features_test))
+# print("Actual score: ",label_test.unique())
--- data.py
+++ data.py
@@ -2,9 +2,14 @@
 question_bank =  [
     {
         'question' : 'What do you understand by the term AI?',
-        'file' : 'q1.csv',
+        'file' : 'question2.csv',
         'hint' : 'AI is being used in our chatbot'
     }
+    # {
+    #     'question' : 'What do you understand by the term AI?',
+    #     'file' : 'q1.csv',
+    #     'hint' : 'AI is being used in our chatbot'
+    # }
 ]
 
 replies = [
